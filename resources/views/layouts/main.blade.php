e95ef0384bd90f52af33b907549bc6cee00a37df
Mark Cameron <mark.cameron@devfactory.ch>
2019-04-04 21:58:12 +0200

    Update photos and add Google Analytics Code

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,13 +2,13 @@
 <html class="no-js">
   <head>
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-114931811-1"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-73553423-2"></script>
     <script>
      window.dataLayer = window.dataLayer || [];
      function gtag(){dataLayer.push(arguments);}
      gtag('js', new Date());
 
-     gtag('config', 'UA-114931811-1');
+     gtag('config', 'UA-73553423-2');
     </script>
 
     <!-- Basic Page Needs
